cf276dea531bfe7e212e81055e082b108dd06346
TheDeathFar <91760248+TheDeathFar@users.noreply.github.com>
2023-07-17 20:32:38 +0300

    minor changes

--- terraform/k8s-cluster-account.tf
+++ terraform/k8s-cluster-account.tf
@@ -17,7 +17,7 @@ resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "load-balancer-admin" {
-  # Сервисному аккаунту назначается роль "vpc.publicAdmin".
+  # Сервисному аккаунту назначается роль "load-balancer.admin".
   folder_id = var.folder_id
   role      = "load-balancer.admin"
   member    = "serviceAccount:${yandex_iam_service_account.k8s-cluster.id}"
--- terraform/k8s-node-account.tf
+++ terraform/k8s-node-account.tf
@@ -3,14 +3,14 @@ resource "yandex_iam_service_account" "k8s-node-group" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
-  # Сервисному аккаунту назначается роль "k8s.clusters.agent".
+  # Сервисному аккаунту назначается роль "container-registry.images.puller".
   folder_id = var.folder_id
   role      = "container-registry.images.puller"
   member    = "serviceAccount:${yandex_iam_service_account.k8s-node-group.id}"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "images-pusher" {
-  # Сервисному аккаунту назначается роль "vpc.publicAdmin".
+  # Сервисному аккаунту назначается роль "container-registry.images.pusher".
   folder_id = var.folder_id
   role      = "container-registry.images.pusher"
   member    = "serviceAccount:${yandex_iam_service_account.k8s-node-group.id}"
